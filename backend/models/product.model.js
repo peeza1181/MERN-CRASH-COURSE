b291dc7455a7e2af187aed2513975c3fa0709f71
HelloReact <640101030029@dpu.ac.th>
2024-10-14 17:06:04 +0700

    Database Setup with MongoDB

---
+++ backend/models/product.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
