de10209808824e4c65e233f306d77da68e3258fb
HelloReact <640101030029@dpu.ac.th>
2024-10-19 17:11:16 +0700

    Building Get, Create, Delete, Update Product API

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 
-app.post("/products", (req, res) => {});
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
